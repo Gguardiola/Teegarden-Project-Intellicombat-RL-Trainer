e75fb51f14491fde876e90a63ac1bf819ebf2beb
gguardiola <gabriel.guardiola1999@gmail.com>
2025-07-08 19:19:41 +0200

    feat: send model to minio updated

--- intellicombat_rl_trainer/scripts/send_model.py
+++ intellicombat_rl_trainer/scripts/send_model.py
@@ -1,3 +1,5 @@
+import os
+import glob
 from minio import Minio
 from minio.error import S3Error
 
@@ -8,16 +10,34 @@ client = Minio(
     secure=False
 )
 
-bucket_name = "models"
-found = client.bucket_exists(bucket_name)
-if not found:
-    client.make_bucket(bucket_name)
-    print(f"bucket created: {bucket_name}")
-else:
-    print(f"Bucket {bucket_name} already exists.")
+BUCKET_NAME = "models"
+MODEL_DIR = "../model"
 
-client.fput_object(
-    bucket_name,         
-    "intellicombat_model_ready.onnx",
-    "../model/intellicombat_model_ready.onnx"
-)
+files = glob.glob(os.path.join(MODEL_DIR, "intellicombat_model_ready_*.onnx"))
+if not files:
+    raise FileNotFoundError("No ONNX model found in local directory!")
+
+model_path = files[0]
+model_filename = os.path.basename(model_path)
+
+print(f"Found: {model_filename}")
+
+if not client.bucket_exists(BUCKET_NAME):
+    client.make_bucket(BUCKET_NAME)
+    print(f"Bucket '{BUCKET_NAME}' created")
+
+try:
+    client.fput_object(
+        BUCKET_NAME,
+        model_filename,
+        model_path
+    )
+    print(f"Uploaded {model_filename} to MinIO")
+
+    os.remove(model_path)
+    print(f"Deleted local file {model_filename}")
+
+except S3Error as e:
+    print(f"Upload failed: {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
