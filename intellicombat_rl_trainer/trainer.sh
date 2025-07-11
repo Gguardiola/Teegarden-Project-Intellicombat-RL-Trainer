f23c10d4ae4bf949b46cbf1a58c7c969ed43f535
Gguardiola <gabriel.guardiola1999@gmail.com>
2025-07-11 18:23:13 +0200

    refactor: trainer updated

--- intellicombat_rl_trainer/trainer.sh
+++ intellicombat_rl_trainer/trainer.sh
@@ -3,23 +3,25 @@ echo "==============================================="
 echo "====      INTELLICOMBAT MODEL TRAINER     ====="
 echo "==============================================="
 echo ""
-echo "- Before starting:"
-echo "-- Remember to populate the /training_simulated_log folder and entries into MINIO collection in order to train and create a consistent AI model."
 
 set -e
 
 cd /code/app/intellicombat_rl_trainer
-PYTHON=$(which python3)
+
+PYTHON="/usr/local/bin/python3"
+
+echo "Using Python: $PYTHON"
+
 echo "Preprocessing data..."
-python3 scripts/preprocess.py
+$PYTHON scripts/preprocess.py
 
 echo "Training the model..."
-python3 scripts/train_model.py
+$PYTHON scripts/train_model.py
 
 echo "Exporting to ONNX..."
-python3 scripts/export_model.py
+$PYTHON scripts/export_model.py
 
 echo "Sending to S3(MINIO)..."
-python3 scripts/send_model.py
+$PYTHON scripts/send_model.py
 
 echo "ALL DONE!!"
